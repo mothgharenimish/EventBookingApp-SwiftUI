06a4acf7ff73e25495b4cc5331da33fa3addd026
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-20 12:21:50 +0530

    Initial Commit

---
+++ EventBookingApp-SwiftUI/EventBookingApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventBookingApp_SwiftUIApp.swift
+//  EventBookingApp-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct EventBookingApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventBookingApp-SwiftUITests/EventBookingApp_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  EventBookingApp_SwiftUITests.swift
+//  EventBookingApp-SwiftUITests
+//
+//  Created by Nimish Sharad Mothghare on 20/03/25.
+//
+
+import Testing
+@testable import EventBookingApp_SwiftUI
+
+struct EventBookingApp_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

290b1d10027e6925f886ebb4f3e618f1843fd941
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-10 16:52:10 +0530

    Xcode Update Changes

---
+++ EventBookingApp-SwiftUI/View/Home/CustomCongratsAlert.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCongratsAlert.swift
+//  EventBookingApp-SwiftUI
+//
+//  Created by Nimish Mothghare on 08/04/25.
+//
+
+import SwiftUI
+
+struct CustomCongratsAlert: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCongratsAlert()
+}
